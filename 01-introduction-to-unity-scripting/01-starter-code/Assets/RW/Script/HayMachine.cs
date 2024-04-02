5f191e4a78c4b6f5676a7cbf77a74ef3e4028c04
Krittapas Chinthumrucks (1000112540) <CHINK3@student.op.ac.nz>
2024-04-03 09:45:11 +1300

    commit

---
+++ 01-introduction-to-unity-scripting/01-starter-code/Assets/RW/Script/HayMachine.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HayMachine : MonoBehaviour
+{
+    public float movementSpeed = 4f;
+    public GameObject hayBalePrefab;
+    public Transform haySpawnpoint;
+    public float shootInterval;
+    public float horizontalBoundary = 22;
+    private float shootTimer;
+
+    void Update()
+    {
+        PerformMovement();
+        HandleShooting();
+    }
+
+    void PerformMovement()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+
+            if (horizontalInput < 0 && transform.position.x > -horizontalBoundary) 
+            {
+                transform.Translate(transform.right * -movementSpeed * Time.deltaTime);
+            }
+            else if (horizontalInput > 0 && transform.position.x < horizontalBoundary) 
+            {
+                transform.Translate(transform.right * movementSpeed * Time.deltaTime);
+            }
+    }
+
+    void HandleShooting()
+    {
+        shootTimer -= Time.deltaTime;
+
+        if (shootTimer <= 0 && Input.GetKey(KeyCode.Space))
+        {
+            shootTimer = shootInterval;
+            ShootHay();
+        }
+    }
+
+    private void ShootHay()
+    {
+        Instantiate(hayBalePrefab, haySpawnpoint.position, Quaternion.identity);
+    }
+}
+
+
